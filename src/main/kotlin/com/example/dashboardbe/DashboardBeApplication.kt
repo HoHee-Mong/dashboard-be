34bb911996faf24e401abf4422a2123b20e22688
HoHee-Mong <didtmdgh0917@gmail.com>
2026-03-11 15:26:11 +0900

    chore: 코틀린/스프링부트 초기 프로젝트 세팅

---
+++ src/main/kotlin/com/example/dashboardbe/DashboardBeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dashboardbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DashboardBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<DashboardBeApplication>(*args)
+}
